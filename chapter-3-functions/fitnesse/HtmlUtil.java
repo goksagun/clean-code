49b07860d547358d59f44a7c7bf74103dec5ed04
Burak Bolat <burak.bolat@hprotravel.com>
2022-08-17 10:32:24 +0300

    Extract setup page and teardown page to single method

--- chapter-3-functions/fitnesse/HtmlUtil.java
+++ chapter-3-functions/fitnesse/HtmlUtil.java
@@ -1,3 +1,4 @@
+package fitnesse;
 /**
  * HtmlUtil
  */
@@ -15,14 +16,18 @@ public class HtmlUtil {
     public static String testableHtml(PageData pageData, boolean isSuite) throws Exception {
         boolean isTestPage = pageData.hasAttritube("Test");
         if (isTestPage) {
-            WikiPage testPage = pageData.getWikiPage();
-            StringBuffer newPageContent = new StringBuffer();
-            includeSetupPages(testPage, newPageContent, isSuite);
-            newPageContent.append(pageData.getContent());  
-            includeTeardownPages(testPage, newPageContent, isSuite);
-            pageData.setContent(newPageContent.toString());
+            includeSetupAndTeardownPages(pageData, isSuite);
         }
         return pageData.getHtml();
+    }
+
+    private static void includeSetupAndTeardownPages(PageData pageData, boolean isSuite) {
+        WikiPage testPage = pageData.getWikiPage();
+        StringBuffer newPageContent = new StringBuffer();
+        includeSetupPages(testPage, newPageContent, isSuite);
+        newPageContent.append(pageData.getContent());
+        includeTeardownPages(testPage, newPageContent, isSuite);
+        pageData.setContent(newPageContent.toString());
     }
 
     private static void includeSetupPages(WikiPage testPage, StringBuffer newPageContent, boolean isSuite) {
--- chapter-3-functions/fitnesse/PageCrawlerImpl.java
+++ chapter-3-functions/fitnesse/PageCrawlerImpl.java
@@ -1,3 +1,4 @@
+package fitnesse;
 
 public class PageCrawlerImpl implements PageCrawler {
 
