eb12a568029c9898cc6a3ad186e171f77be68812
Burak Bolat <burak.bolat@hprotravel.com>
2022-09-22 20:57:22 +0300

    Create a new class PrimeGenerator and add its description

---
+++ chapter-4-comments/sieve/PrimeGenerator.java
@@ -0,0 +1,13 @@
+package sieve;
+
+/**
+ * This class generates prime numbers up to a user specified 
+ * maximum. The algorithm used is the Sieve of Eratosthenes.
+ * Given an array of integers starting at 2:
+ * Find the first uncrossed integer, and cross out all its 
+ * multiples. Repeat until there are no more multiples
+ * in the array.
+ */
+public class PrimeGenerator {
+    
+}
